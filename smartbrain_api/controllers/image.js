aa068fe67c0cf9b6613756a5f2e7eea7a7a27037
pranavshrikhande <pranav.shrikhande@gmail.com>
2019-07-08 16:30:04 -0400

    server working

---
+++ smartbrain_api/controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey:'92f124725bfe4d6ca517bf07a7454028'
+  });
+
+
+  const handleApiCall= (req, res) => {
+      app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data=>{
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req,res,db)=>{
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries',1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+    }
+
+    module.exports={
+        handleImage,
+        handleApiCall
+    }
